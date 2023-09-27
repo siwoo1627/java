bffbc208a5f9dd82c11dc81faffb6be7b8087568
psw <1541xm@gmail.com>
2023-09-28 01:19:31 +0900

    한글수정

--- LearningToJava/src/Gugudan.java
+++ LearningToJava/src/Gugudan.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Gugudan {
 
 	public static void main(String[] args) {
-		// ������ �Է��ϱ�(�ݺ��� ���)22222
+		// 구구단 입력하기(반복문 사용) 
 		Scanner scanner = new Scanner(System.in);
-		System.out.print("���ڸ� �Է��ϼ���: ");
+		System.out.print("숫자를 입력하세요: ");
 		int num = scanner.nextInt();
 		
 		for(int i=1; i<10; i++) {
