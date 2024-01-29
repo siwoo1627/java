f108aa4410c33f9a7d3082abc9e3ece2314edc33
local_siwoo <1541xm@naver.com>
2024-01-29 23:37:23 +0900

    no message

---
+++ Servlet/책정보/BookTest2Servlet.java
@@ -0,0 +1,24 @@
+package com.edu.test;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/bookOutput")
+public class BookTest2Servlet extends HttpServlet {
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = resp.getWriter();
+
+		Book book = (Book)req.getAttribute("book");
+		
+		out.print("<h3>책제목 : "+book.getTitle()+"</h3>");
+		out.print("<h3>책저자 : "+book.getAuthor()+"</h3>");
+		out.print("<h3>출판사 : "+book.getPublisher()+"</h3>");
+		
+
+		out.close();
+	}
+}
